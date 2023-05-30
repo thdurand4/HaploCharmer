8413b0a41cefb26a40583edf2955b77a23f8a30a
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 15:53:07 +0200

    add condition on module

--- Haplocharmer/module.py
+++ Haplocharmer/module.py
@@ -21,3 +21,32 @@ class HaploCharmer(SnakEcdysis):
         # print(workflow.__dict__)
 
         # Initialisation of Haplocharmer attributes
+        
+    def __build_tools_activated(self, key, allow, mandatory=False):
+        tools_activate = []
+        for tool, activated in self.config[key].items():
+            if tool in allow:
+                boolean_activated = var_2_bool(key, tool, activated)
+                if boolean_activated:
+                    tools_activate.append(tool)
+                    self.config[key][tool] = boolean_activated
+                    self.__check_tools_config(tool, [tool])
+            else:
+                raise ValueError(f'CONFIG FILE CHECKING FAIL : {key} {tool} not allow on iKISS"')
+        if len(tools_activate) == 0 and mandatory:
+            raise ValueError(f"CONFIG FILE CHECKING FAIL : you need to set True for at least one {key} from {allow}")
+        return tools_activate
+
+
+    def __var_2_bool(self, key, tool, to_convert):
+        """convert to boolean"""
+        if isinstance(type(to_convert), bool):
+            return to_convert
+        elif f"{to_convert}".lower() in ("yes", "true", "t"):
+            return True
+        elif f"{to_convert}".lower() in ("no", "false", "f"):
+            return False
+        else:
+            raise TypeError(
+                f'CONFIG FILE CHECKING FAIL : in the "{key}" section, "{tool}" key: "{to_convert}" is not a valid boolean')
+
