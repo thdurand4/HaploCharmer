eb7d7394824a06d08455e2551f1e64d634ed8f2e
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 13:47:21 +0200

    Add git ignore

--- Haplocharmer/global_variables.py
+++ Haplocharmer/global_variables.py
@@ -4,11 +4,11 @@ DOCS = "https://Haplocharmer.readthedocs.io/en/latest/"
 GIT_URL = "https://github.com/thdurand4/HaploCharmer"
 
 INSTALL_PATH = Path(__file__).resolve().parent
-#SINGULARITY_URL_FILES = [('https://itrop.ird.fr/culebront_utilities/singularity_build/Singularity.culebront_tools.sif',
-#                          'INSTALL_PATH/containers/Singularity.culebront_tools.sif'),
-#                         ('https://itrop.ird.fr/culebront_utilities/singularity_build/Singularity.report.sif',
-#                          'INSTALL_PATH/containers/Singularity.report.sif')
-#                         ]
+SINGULARITY_URL_FILES = [('https://itrop.ird.fr/culebront_utilities/singularity_build/Singularity.culebront_tools.sif',
+                          'INSTALL_PATH/containers/Singularity.culebront_tools.sif'),
+                         ('https://itrop.ird.fr/culebront_utilities/singularity_build/Singularity.report.sif',
+                          'INSTALL_PATH/containers/Singularity.report.sif')
+                         ]
 
 DATATEST_URL_FILES = ("https://itrop.ird.fr/culebront_utilities/Data-Xoo-sub.zip", "Data-Xoo-sub.zip")
 SCRIPTS = INSTALL_PATH.joinpath("scripts")
